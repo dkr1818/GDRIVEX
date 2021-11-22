baec619b61d787ee635de1d52406e42160e83b6e
Code X Mania <urluploaderadarsh@gmail.com>
2021-11-22 19:40:25 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -41,8 +41,8 @@ def map(pos):
 
         button = [
             [
-             InlineKeyboardButton(text = 'Support Chat', url = "https://t.me/lkhitech"),
-             InlineKeyboardButton(text = 'Feature Request', url = "https://github.com/kjeymax/GDUPLOAD_BOT2")
+             InlineKeyboardButton(text = 'Support Chat', url = "https://t.me/codexmania"),
+             InlineKeyboardButton(text = 'Feature Request', url = "https://t.me/adarsh_goel")
             ],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
 
